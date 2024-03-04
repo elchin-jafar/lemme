92672910c77684e523bf10059c7241fcb2dd7117
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2024-03-04 19:36:02 +0400

    Popup integration added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'box': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+      },
+    },
   },
   plugins: [],
 };
