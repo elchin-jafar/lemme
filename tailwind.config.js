7a902c479af9de1df92263b21cd9c0e34e89a43c
Elchin <elcinceferli@gmail.com>
2024-03-05 22:14:21 +0400

    add testpage & tailwind-plugin for hidden scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        'box': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+        box: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
