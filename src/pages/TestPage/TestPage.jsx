1eb3c05660d738635a21ab9da7197f42d1ecb984
Elchin <elcinceferli@gmail.com>
2024-03-06 18:43:21 +0400

    add answers functionality

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -2,6 +2,7 @@ import logo from "../../assets/logo2.png";
 import model_S from "../../assets/models/modelS.png";
 import model_M from "../../assets/models/modelM.png";
 import model_L from "../../assets/models/modelL.png";
+import { useState } from "react";
 
 const questions = [
   {
@@ -31,6 +32,13 @@ const questions = [
 ];
 
 function TestPage() {
+  const [q, setQ] = useState({});
+
+  function handleAnswer(questionIndex, answerIndex = 0) {
+    const key = `s${questionIndex + 1}`;
+    const value = answerIndex + 1;
+    setQ((prevState) => ({ ...prevState, [key]: value }));
+  }
   return (
     <div className="bg-gradient-to-r from-[#E5F3FF] to-[#D8E3FF] w-screen h-screen pt-20 pl-28 pb-[7rem]">
       <h2 className="text-[6rem] font-bold">
@@ -45,28 +53,24 @@ function TestPage() {
         Burada 4 dəri növündən sizə hansının məxsus olacağını biləcəyik:
       </p>
 
-      <div className="max-h-[60%] flex 2xl:gap-[20rem]">
+      <div className="max-h-[60%] flex gap-[10rem] 2xl:gap-[20rem]">
         <div className="overflow-y-scroll scrollbar-hide  max-w-[50%] mt-[3.5rem]">
-          {questions.map((q, i) => (
-            <div key={i}>
-              <p className="text-[2.5rem] font-bold">{q.question}</p>
-              <div className="flex gap-x-[1.5rem]">
-                {q.answers.map((a, i) => (
-                  <p
-                    key={i}
-                    className="text-[2rem] font-normal py-[1rem] px-[2rem] rounded-[3.5rem] bg-white w-fit"
-                  >
-                    {a}
-                  </p>
-                ))}
-              </div>
-            </div>
+          {questions.map((q, index) => (
+            <Question
+              q={q}
+              index={index}
+              handleAnswer={handleAnswer}
+              key={index}
+            />
           ))}
         </div>
         <Models />
       </div>
 
-      <div className="px-[2.9rem] py-[1rem] text-[2rem] font-bold text-white bg-[#56A8FF] rounded-[1.5rem] w-fit cursor-pointer mt-[4rem]">
+      <div
+        onClick={() => console.log(q)}
+        className="px-[2.9rem] py-[1rem] text-[2rem] font-bold text-white bg-[#56A8FF] rounded-[1.5rem] w-fit cursor-pointer mt-[4rem]"
+      >
         Cavabı al
       </div>
     </div>
@@ -78,15 +82,45 @@ export default TestPage;
 function Models() {
   return (
     <>
-      <div className="relative w-[61rem] h-[55rem] 2xl:w-[61rem] 2xl:h-[55rem] border border-yellow-400">
-        <div className="p-[3rem] border border-black rounded-full w-max bg-[#F6EFF2] absolute left-[50%] -translate-x-1/2 bottom-0 z-[3]">
+      <div className="relative w-[55rem] h-[48rem] 2xl:w-[61rem] 2xl:h-[55rem] border border-yellow-400">
+        <div className="p-[3rem] border border-black rounded-full w-[26rem] 2xl:w-max bg-[#F6EFF2] absolute left-[50%] -translate-x-1/2 bottom-0 z-[3]">
           <img src={model_S} alt="" />
         </div>
-        <div className="p-[3rem] border border-black rounded-full w-max bg-[#F6EFF2] absolute left-0 2xl:-top-[2rem] z-[2]">
+        <div className="p-[3rem] border border-black rounded-full w-[29rem] 2xl:w-max bg-[#F6EFF2] absolute left-0 -top-[2rem] z-[2]">
           <img src={model_M} alt="" />
         </div>
-        <div className="p-[3rem] border border-black rounded-full w-max bg-[#F6EFF2] absolute right-0 top-0 z-[1]">
+        <div className="p-[3rem] border border-black rounded-full w-[36rem] 2xl:w-max bg-[#F6EFF2] absolute right-0 top-0 z-[1]">
           <img src={model_L} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Question({ q, index, handleAnswer }) {
+  const [selected, setSelected] = useState();
+  function handleClick(index, i) {
+    handleAnswer(index, i);
+    setSelected(i);
+  }
+  return (
+    <>
+      <div>
+        <p className="text-[2.5rem] font-bold">{q.question}</p>
+        <div className="flex gap-x-[1.5rem]">
+          {q.answers.map((a, i) => (
+            <p
+              key={i}
+              className={`text-[2rem] font-normal py-[1rem] px-[2rem] cursor-pointer rounded-[3.5rem] w-fit ${
+                selected == i
+                  ? "bg-[#EFA0C6] text-white"
+                  : "bg-white text-black"
+              }`}
+              onClick={() => handleClick(index, i)}
+            >
+              {a}
+            </p>
+          ))}
         </div>
       </div>
     </>
