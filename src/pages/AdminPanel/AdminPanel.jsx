92044f4fce6abcbf2b2c7e072d3ed6eba5b9d54d
Elchin <elcinceferli@gmail.com>
2024-03-07 20:20:24 +0400

    add 2nd modal

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -4,8 +4,12 @@ import { Button, Modal } from "antd";
 // import Link from "antd/es/typography/Link";
 import { Popover } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import {
+  faThumbsDown,
+  faThumbsUp,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 
 const getRandomInt = (max, min = 0) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
@@ -54,94 +58,70 @@ function AdminPanel() {
         Open Modal with async logic
       </Button>
       <Modal
-        title="Dəri tipinizi qeyd edin"
+        title="Nəticə"
         open={open}
         onOk={handleOk}
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
         width={800}
-        footer={[
-          <div className="pt-8 md:pt-[37px] flex justify-center items-center">
-            <Link to="/productPage/1">
-              <button
-                onClick={() => setOpen(false)}
-                className="px-5 py-2.5 bg-[#56A8FF] hover:bg-[#56A8FF] shadow-lg hover:shadow-box rounded-xl text-white transition duration-300 ease-in-out transform hover:scale-105"
-              >
-                Davam et
-              </button>
-            </Link>
-          </div>,
-        ]}
+        footer={[]}
       >
-        {/* <div className="relative md:max-w-[840px] w-full md:p-10 h-auto shadow-box bg-white px-5 md:px-[60px] pt-[50px] pb-10 rounded-xl z-10"> */}
-        {/* <div className="flex gap-5 justify-between pb-5 md:pb-10 items-center">
-          <h1 className="font-bold text-xl md:text-2xl">
-            Dəri tipinizi qeyd edin
-          </h1>
+        {/* <div className="flex justify-between pb-[30px] items-center">
+          <h1 className="font-bold text-2xl">Nəticə</h1>
           <Link
             to="/"
-            className=" w-[30px] h-[30px] md:w-[50px] md:h-[50px] bg-[#d9d9d9] hover:bg-white flex justify-center items-center rounded-full shadow-btn hover:shadow-box"
+            className="w-[50px] h-[50px] bg-[#d9d9d9] hover:bg-white flex justify-center items-center rounded-full shadow-btn hover:shadow-box"
           >
             <FontAwesomeIcon
-              className="text-[20px] md:text-[30px] text-[#828282]"
+              className="text-[30px] text-[#828282]"
               icon={faTimes}
             />
           </Link>
         </div> */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-10">
-          <Link
-            to="/productPage/1"
-            className="px-2.5 pt-2.5 group w-full h-[205px] bg-[#eee] hover:bg-white rounded-xl shadow-lg hover:shadow-box flex flex-col justify-center items-center transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            <img
-              className="max-w-[140px] w-full max-h-[140px]  object-cover mb-3"
-              src="../src/assets/Rectangle 4.svg"
-              alt=""
-            />
-            <p className="font-bold text-lg md:text-xl text-center">Quru</p>
-          </Link>
-          <Link
-            to="/productPage/1"
-            className="px-2.5 pt-2.5 group w-full h-[205px] bg-[#eee] hover:bg-white rounded-xl shadow-lg hover:shadow-box flex flex-col justify-center items-center transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            <img
-              className="max-w-[140px] w-full max-h-[140px] object-cover mb-3"
-              src="../src/assets/Rectangle 1.svg"
-              alt=""
-            />
-            <p className="font-bold text-lg md:text-xl text-center">Yağlı</p>
-          </Link>
-          <Link
-            to="/productPage/1"
-            className="px-2.5 pt-2.5 group w-full h-[205px] bg-[#eee] hover:bg-white rounded-xl shadow-lg hover:shadow-box flex flex-col justify-center items-center transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            <img
-              className="max-w-[140px] w-full max-h-[140px] object-cover mb-3"
-              src="../src/assets/Rectangle 3.svg"
-              alt=""
-            />
-            <p className="font-bold text-lg md:text-xl text-center">Qarışıq</p>
-          </Link>
-          <Link
-            to="/productPage/1"
-            className="px-2.5 pt-2.5 group w-full h-[205px] bg-[#eee] hover:bg-white rounded-xl shadow-lg hover:shadow-box flex flex-col justify-center items-center transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            <img
-              className="max-w-[140px] w-full max-h-[140px] object-cover mb-3"
-              src="../src/assets/Rectangle 2.svg"
-              alt=""
-            />
-            <p className="font-bold text-lg md:text-xl text-center">Bilmirəm</p>
-          </Link>
+        <div className="px-2.5">
+          <p className="text-lg">
+            Sizin dəri tipiniz qurudur. Bu mərhələdən sonra sizin baxış
+            keçirdiyiniz məhsullarda əgər sizin dərinizə uyğundursa{" "}
+            <span className="text-[#50AB64] underline">Yaşıl</span> əksinə uyğun
+            deyilsə <span className="text-[#ff3100] underline">Qırmızı </span>
+            teq ilə işarə olunacaqdır.
+          </p>
+          <h1 className="font-bold text-lg pt-5">Teqler:</h1>
+          <div className="flex gap-[15px] pt-3">
+            <span className="px-5 py-2.5 rounded-lg text-white bg-[#50AB64]">
+              dəri tipinizə uyğundur
+            </span>
+            <span className="px-5 py-2.5 rounded-lg text-white bg-[#FF3100]">
+              dəri tipinizə uyğun deyil
+            </span>
+          </div>
         </div>
-        {/* <div className="pt-8 md:pt-[37px] flex justify-center items-center">
+        <div className="py-[35px] bg-[#000] bg-opacity-20 rounded-xl mt-[30px] shadow-box">
+          <h1 className="text-center text-xl font-bold px-2 md:text-2xl sm:text-xl">
+            Bu məlumat sizə faydalı oldumu?
+          </h1>
+          <div className="flex justify-center items-center gap-[35px] pt-5">
+            <button>
+              <FontAwesomeIcon
+                className="text-[54px] text-[#50AB64]"
+                icon={faThumbsUp}
+              />
+            </button>
+            <button>
+              <FontAwesomeIcon
+                className="text-[54px] text-[#FF3100]"
+                icon={faThumbsDown}
+              />
+            </button>
+          </div>
+        </div>
+        <div className="pt-[37px] flex justify-center items-center">
           <Link>
-            <button className="px-5 py-2.5 bg-[#56A8FF] hover:bg-[#56A8FF] shadow-lg hover:shadow-box rounded-xl text-white transition duration-300 ease-in-out transform hover:scale-105">
-              Davam et
+            <button className="px-5 py-2.5 bg-[#56A8FF] hover:bg-[#5095df] shadow-lg hover:shadow-box rounded-xl text-white">
+              Ana menyuya qayıt
             </button>
           </Link>
-        </div> */}
-        {/* </div> */}
+        </div>
       </Modal>
     </>
   );
