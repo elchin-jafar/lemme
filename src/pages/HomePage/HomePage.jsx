4f84e4eadd3a09fc974cfdf202a22d47ca4888c0
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2024-03-11 21:56:23 +0400

    Fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import logo2 from "../../assets/logo2.png";
 import { Profile, SearchNormal1 } from "iconsax-react";
 import { Link } from "react-router-dom";
 import { useSkinTypeModalStore } from "../../store/skinTypeModalStore";
-import { Fragment, useState, useCallback } from "react";
+import { Fragment, useState, useCallback, useEffect, useRef } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import SkinTypeModal from "../../components/SkinTypeModal/SkinTypeModal";
 import { getProductByName } from "../../utils/apiUtils";
@@ -22,6 +22,25 @@ function HomePage() {
   const [selected, setSelected] = useState(0);
   // const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+  const [showPlaceholder, setShowPlaceholder] = useState(false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleInputClick = () => {
+    setShowPlaceholder(true);
+  };
+
+  const handleClickOutside = (event) => {
+    if (inputRef.current && !inputRef.current.contains(event.target)) {
+      setShowPlaceholder(false);
+    }
+  };
 
   const {
     isOpen: isModalOpen,
@@ -83,14 +102,15 @@ function HomePage() {
         />
       </div> */}
 
-      <div className="relative z-20 bg-[#EFA0C6] sm:w-[60.4rem] w-[300px] h-[100px] sm:h-[10rem] rounded-[12.6rem] mb-5 flex justify-center items-center">
+      <div className="relative z-20 bg-transparent sm:w-[60.4rem]  h-[100px] sm:h-[9.1rem] rounded-[12.6rem] mb-5 flex justify-center items-center">
         <Combobox value={selected} onChange={setSelected}>
           <div className="mt-1">
             <div className="bg-[#EFA0C6] text-[#2B2727] rounded-[12.6rem] placeholder-[#2B2727] text-center text-[20px]  sm:text-[3.6rem] outline-none max-w-[80%]">
               <Combobox.Input
-                className="bg-[#EFA0C6] text-[#2B2727] h-[9.6rem] rounded-[12.6rem] placeholder-[#2B2727] text-center text-[20] sm:text-[3.6rem] outline-none"
-                placeholder="Məhsulun adı"
-                // displayValue={(person) => person.name}
+                ref={inputRef}
+                className="bg-[#EFA0C6] text-[#2B2727] sm:w-[60.4rem] w-[300px] h-[100px] sm:h-[9.6rem] rounded-[12.6rem] placeholder-[#2B2727] text-center text-[20] sm:text-[3.6rem] focus:bg-[#EFA0C6] outline-none"
+                placeholder={showPlaceholder ? "" : "Məhsulun adı"}
+                onClick={handleInputClick}
                 onChange={(e) => debounceOnChange(e.target.value)}
               />
               <Combobox.Button className="absolute inset-y-0 right-24 flex items-center">
@@ -104,9 +124,9 @@ function HomePage() {
               leaveTo="opacity-0"
               // afterLeave={() => setQuery("")}
             >
-              <Combobox.Options className="absolute left-0 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              <Combobox.Options className="absolute left-0 mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                 {results?.length === 0 ? (
-                  <div className="relative cursor-not-allowed text-3xl select-none px-4 py-2 text-gray-700">
+                  <div className="relative cursor-not-allowed text-3xl select-none px-4 py-2  bg-white">
                     Nothing found.
                   </div>
                 ) : (
