73bc2695b66e8398160a093b5bc1fdf0c4e8b602
Elchin <elcinceferli@gmail.com>
2024-03-26 18:22:48 +0400

    change var

--- src/utils/apiUtils.jsx
+++ src/utils/apiUtils.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import endpoints from "../api/endpoints";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
